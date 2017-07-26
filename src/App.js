bd5c2038374a9e0b1e7524860d46dc27fb8a6145
Zach Payton <paytonzk@rose-hulman.edu>
2017-07-25 22:18:41 -0700

    Added a tabs section to go to different pages in the future

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ class App extends Component {
         <div className="App-header">
           <h1 className="header1">PVP Parts and Service</h1>
         </div>
+        <div className="App-tabs">
+          <form className="tab-pages">
+            <ul>
+              <li><button className="button">Home</button></li>
+              <li><button className="button">About Us</button></li>
+              <li><button className="button">Inventory</button></li>
+              <li><button className="button">FAQ</button></li>
+            </ul>
+          </form>
+          </div>
         
       </div>
     );
